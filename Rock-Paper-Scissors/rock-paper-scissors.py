41e0856db97dba4104feae683ca5a6171dc8c338
AnaNikki <125910304+AnaNikki@users.noreply.github.com>
2025-02-24 20:35:50 +0100

    Added Stage 3 - Endless game
    Update rock-paper-scissors.py with an endless game.

--- Rock-Paper-Scissors/rock-paper-scissors.py
+++ Rock-Paper-Scissors/rock-paper-scissors.py
@@ -1,22 +1,52 @@
 # Stage 1 - Unfair computer
 # Stage 2 - Equalizing chances
+# Stage 3 - Endelss game
 
 import random
 
-if user_option == "rock" or user_option == "paper" or user_option == "scissors":
-    if user_option == random_option:
-        print(f"There is a draw ({random_option})")
-    elif user_option == "rock" and random_option == "paper":
-        print(f"Sorry, but the computer chose {random_option}")
-    elif user_option == "rock" and random_option == "scissors":
-        print(f"Well done. The computer chose {random_option} and failed")
-    elif user_option == "paper" and random_option == "rock":
-        print(f"Well done. The computer chose {random_option} and failed")
-    elif user_option == "paper" and random_option == "scissors":
-        print(f"Sorry, but the computer chose {random_option}")
-    elif user_option == "scissors" and random_option == "rock":
-        print(f"Sorry, but the computer chose {random_option}")
-    elif user_option == "scissors" and random_option == "paper":
-        print(f"Well done. The computer chose {random_option} and failed")
-else:
-    print("Please choose between rock, paper, or scissors!")
+while True:
+    # Get the user's input and convert it to lowercase for case-insensitive comparison
+    user_option = input("Choose rock, paper, scissors, or '!exit' for exiting the game: ").lower()
+
+    # Define valid options for the game
+    computer_option = ["rock", "paper", "scissors"]
+
+    # Randomly choose the computer's option
+    random_option = random.choice(computer_option)
+
+    # Check if the user input is valid
+    if user_option == "rock" or user_option == "paper" or user_option == "scissors":
+
+        # Define dictionary with the outcomes
+        # First key = user input
+        outcomes = {
+            "rock": {
+                "rock": f"There is a draw (rock)",
+                "paper": f"Sorry, but the computer chose paper",
+                "scissors": f"Well done. The computer chose scissors and failed"
+            },
+            "paper": {
+                "rock": f"Well done. The computer chose rock and failed",
+                "paper": f"There is a draw (paper)",
+                "scissors": f"Sorry, but the computer chose scissors"
+            },
+            "scissors": {
+                "rock": f"Sorry, but the computer chose rock",
+                "paper": f"Well done. The computer chose paper and failed",
+                "scissors": f"There is a draw (scissors)",
+            }
+        }
+
+        # Get the result from the dictionary from the user input and the computer choice
+        result = outcomes[user_option].get(random_option)
+        print(random_option)
+        print(result)
+
+    # Check if the user input is !exit
+    elif user_option == "!exit":
+            print("Bye!")
+            break
+
+    else:
+        print("Invalid input")
+        print("Please choose between rock, paper, or scissors! Or '!exit' for exiting the game!")
