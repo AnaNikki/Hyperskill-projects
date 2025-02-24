111f1931057e84aa350c479899ae760f9ab97846
AnaNikki <125910304+AnaNikki@users.noreply.github.com>
2025-02-24 20:29:10 +0100

    Added Stage 2
    Update rock-paper-scissors.py with Stage 2.  Stage 1 is updated with Stage 2.

--- Rock-Paper-Scissors/rock-paper-scissors.py
+++ Rock-Paper-Scissors/rock-paper-scissors.py
@@ -1,15 +1,22 @@
 # Stage 1 - Unfair computer
+# Stage 2 - Equalizing chances
 
-user_option = input("Choose rock, paper, or scissors: ")
+import random
 
-computer_option = ""
-
-if user_option == 'rock':
-    computer_option = 'paper'
-    print(f"Sorry, but the computer chose {computer_option}")
-elif user_option == 'paper':
-    computer_option = 'scissors'
-    print(f"Sorry, but the computer chose {computer_option}")
-elif user_option == 'scissors':
-    computer_option = 'rock'
-    print(f"Sorry, but the computer chose {computer_option}")
+if user_option == "rock" or user_option == "paper" or user_option == "scissors":
+    if user_option == random_option:
+        print(f"There is a draw ({random_option})")
+    elif user_option == "rock" and random_option == "paper":
+        print(f"Sorry, but the computer chose {random_option}")
+    elif user_option == "rock" and random_option == "scissors":
+        print(f"Well done. The computer chose {random_option} and failed")
+    elif user_option == "paper" and random_option == "rock":
+        print(f"Well done. The computer chose {random_option} and failed")
+    elif user_option == "paper" and random_option == "scissors":
+        print(f"Sorry, but the computer chose {random_option}")
+    elif user_option == "scissors" and random_option == "rock":
+        print(f"Sorry, but the computer chose {random_option}")
+    elif user_option == "scissors" and random_option == "paper":
+        print(f"Well done. The computer chose {random_option} and failed")
+else:
+    print("Please choose between rock, paper, or scissors!")
